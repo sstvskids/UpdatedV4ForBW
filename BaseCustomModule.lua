596ebbda8571e93ccb5e72f20fd7346725dbe225
sstvskids <126665568+sstvskids@users.noreply.github.com>
2025-05-24 19:05:16 +1000

    Update BaseCustomModule.lua

--- BaseCustomModule.lua
+++ BaseCustomModule.lua
@@ -72,7 +72,7 @@ local function getcustomassetfunc(path)
 			textlabel:Remove()
 		end)
 		local req = requestfunc({
-			Url = "https://raw.githubusercontent.com/qwertyui-is-back/UpdatedV4ForBW/main/"..path:gsub("vape/assets", "assets"),
+			Url = "https://raw.githubusercontent.com/sstvskids/UpdatedV4ForBW/main/"..path:gsub("vape/assets", "assets"),
 			Method = "GET"
 		})
 		writefile(path, req.Body)
